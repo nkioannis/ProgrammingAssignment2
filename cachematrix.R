26cae45c888ab090668d7cb9163667d2e17a79a6
nkioannis <nkioannis@gmail.com>
2014-12-17 19:43:35 +0200

    Rcode added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,6 @@
+##The following function creates a special "vector" which is really a list
+##containing a function to: set and get the value of a matrix
+##set and get the value of the inverse of a matrix
 makeCacheMatrix<-function(x = numeric()) {
   m<-NULL
   set<-function(y){
@@ -9,6 +12,10 @@ makeCacheMatrix<-function(x = numeric()) {
   getinverse<-function()m
   list(set= set, get = get, setinverse= setinverse, getinverse= getinverse)
 }
+##The following function calculates the inverse of the given matrix using
+##the first function. If the inverse have been calculated
+## (getinverse is not null) then returnes the inverse from the cache
+##else, the function calculates the inverse and save it to the cache
 cacheSolve<-function(x, ...) {
   m<-x$getinverse()
   if(!is.null(m)){
